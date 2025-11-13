40c07577e3b8e57d7c333900dec633f84c1cf561
Mohamed Ibrahim Abdel-Samee Mahmoud <m.mahmoud17996@student.aast.edu>
2025-11-13 22:25:18 +0200

    Upgrade version to V3.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         minSdk = 29
         targetSdk = 36
         versionCode = 1
-        versionName = "3.0.1"
+        versionName = "3.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
